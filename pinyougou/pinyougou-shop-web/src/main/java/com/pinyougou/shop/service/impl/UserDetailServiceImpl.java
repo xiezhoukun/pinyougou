a33a9748e733a47d56971b95bb8e030694211884
xiezhoukun <826761642@qq.com>
2018-11-27 20:58:17 +0800

    pinyougou项目
    更改商家登录认证

--- pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/service/impl/UserDetailServiceImpl.java
+++ pinyougou/pinyougou-shop-web/src/main/java/com/pinyougou/shop/service/impl/UserDetailServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pinyougou.shop.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.pojo.TbSeller;
 import com.pinyougou.sellergoods.service.SellerService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,15 +15,28 @@ import java.util.List;
 
 public class UserDetailServiceImpl implements UserDetailsService {
 
+    //@Reference
     private SellerService sellerService;
 
+    //用户在前端页面输入的用户名
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
-        return new User(username, "123456", authorities);
+        //根据商家id查询商家
+        TbSeller seller = sellerService.findOne(username);
+        //商家存在并且已经审核通过才进行认证
+        if (seller != null && "1".equals(seller.getStatus())) {
+            //构造用户的角色列表
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
+
+            //将使用前端输入的密码与给定的密码进行匹配，如果一致则登录认证成功
+            return new User(username, seller.getPassword(), authorities);
+        }
+
+        return null;
     }
+
     public void setSellerService(SellerService sellerService) {
         this.sellerService = sellerService;
     }
