a349b700cf31ec0f27bc5f47586b76a1c85508c5
xiezhoukun <826761642@qq.com>
2018-12-03 16:05:30 +0800

    启用规格

--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -63,51 +63,9 @@ public class GoodsServiceImpl extends BaseServiceImpl<TbGoods> implements GoodsS
         goods.getGoodsDesc().setGoodsId(goods.getGoods().getId());
         goodsDescMapper.insertSelective(goods.getGoodsDesc());
 
-        //新增商品SKU
-        for (TbItem item : goods.getItemList()) {
+        saveItemList(goods);
 
-            String title = goods.getGoods().getGoodsName();
-            //组合规格选项形成SKU标题
-            Map<String, Object> map = JSON.parseObject(item.getSpec());
-            Set<Map.Entry<String, Object>> entries = map.entrySet();
-            for (Map.Entry<String, Object> entry : entries) {
-                title += " " + entry.getValue().toString();
-            }
 
-            item.setTitle(title);
-
-            //图片
-            List<Map> imgList = JSONArray.parseArray(goods.getGoodsDesc().getItemImages(), Map.class);
-            if (imgList != null && imgList.size() > 0) {
-                //将商品的第一张图作为sku的图片
-                item.setImage(imgList.get(0).get("url").toString());
-            }
-
-            //商品分类id
-            item.setCategoryid(goods.getGoods().getCategory3Id());
-
-            //商品分类名称
-            TbItemCat itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
-            item.setCategory(itemCat.getName());
-
-            //创建时间
-            item.setCreateTime(new Date());
-
-            //更新时间
-            item.setUpdateTime(item.getCreateTime());
-            //SPU商品id
-            item.setGoodsId(goods.getGoods().getId());
-            //商家id
-            item.setSellerId(goods.getGoods().getSellerId());
-            //商家名称
-            TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
-            item.setSeller(seller.getName());
-            //品牌名称
-            TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
-            item.setBrand(brand.getName());
-
-            itemMapper.insertSelective(item);
-        }
     }
 
     @Override
@@ -121,5 +79,73 @@ public class GoodsServiceImpl extends BaseServiceImpl<TbGoods> implements GoodsS
         criteria.andIn("id", Arrays.asList(ids));
         goodsMapper.updateByExampleSelective(goods, example);
 
+    }
+
+    private void saveItemList(Goods goods) {
+
+        if ("1".equals(goods.getGoods().getIsEnableSpec())) {
+
+            //如果启动规格,则需要按照规格生成不同的SKU商品
+            //新增商品SKU
+            for (TbItem item : goods.getItemList()) {
+
+                String title = goods.getGoods().getGoodsName();
+                //组合规格选项形成SKU标题
+                Map<String, Object> map = JSON.parseObject(item.getSpec());
+                Set<Map.Entry<String, Object>> entries = map.entrySet();
+                for (Map.Entry<String, Object> entry : entries) {
+                    title += " " + entry.getValue().toString();
+                }
+
+                item.setTitle(title);
+
+                setItemValue(item, goods);
+
+                itemMapper.insertSelective(item);
+            }
+        } else {
+            //如果没有启动规格,则只存在SKU信息
+            TbItem tbItem = new TbItem();
+            tbItem.setTitle(goods.getGoods().getGoodsName());
+            tbItem.setPrice(goods.getGoods().getPrice());
+            tbItem.setNum(9999);
+            tbItem.setStatus("0");
+            tbItem.setIsDefault("1");
+            tbItem.setSpec("{}");
+
+            itemMapper.insertSelective(tbItem);
+        }
+    }
+
+    private void setItemValue(TbItem item, Goods goods) {
+        //图片
+        List<Map> imgList = JSONArray.parseArray(goods.getGoodsDesc().getItemImages(), Map.class);
+        if (imgList != null && imgList.size() > 0) {
+            //将商品的第一张图作为sku的图片
+            item.setImage(imgList.get(0).get("url").toString());
+        }
+
+        //商品分类id
+        item.setCategoryid(goods.getGoods().getCategory3Id());
+
+        //商品分类名称
+        TbItemCat itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
+        item.setCategory(itemCat.getName());
+
+        //创建时间
+        item.setCreateTime(new Date());
+
+        //更新时间
+        item.setUpdateTime(item.getCreateTime());
+        //SPU商品id
+        item.setGoodsId(goods.getGoods().getId());
+        //商家id
+        item.setSellerId(goods.getGoods().getSellerId());
+        //商家名称
+        TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
+        item.setSeller(seller.getName());
+        //品牌名称
+        TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
+        item.setBrand(brand.getName());
     }
 }
